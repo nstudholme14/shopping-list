c9dbd9c38933eaf4b5a38a1d09f36242ff206a93
Nora Studholme <nstudholme14@cmc.edu>
2015-04-27 08:19:35 -0700

    "this is a commit of my final shopping list!"

--- js/source.js
+++ js/source.js
@@ -2,22 +2,37 @@ $(document).ready(function(){
 
 
 	//use enter to add list items (w/2 boxes)
-	
+	var addItem = function (item) {
+		var addedItem = '<li class="items">' + '<img class="check" src="images/checkicon.png"> ' + '<span>' + item + '<span>' + 
+					'<img class="delete" src="images/deleteicon.png">' + '</li>' ; 
+		$('#list').append(addedItem); 
+			//make list elements .items draggable again
+		$('.items').draggable();
+	} ;
 
 	//use click to add list items (w/2 boxes)
 	$('#add').click(function(){
-		$('.items').append('<input id="item"> </input>')
+		addItem ($('#item').val())
 	});
+
+	$('#item').keyup(function(e) {
+		if (e.which==13) {
+		addItem ($('#item').val())	
+		}	
+	});	
+
 
 	//strike through li when check box clicked
 	$('.check').click(function() {
-		$(this).siblings('span').css('text-decoration', 'line-through');
+		$(this).siblings('span').toggleClass('checked');	
 		}); 
+
+	
 	
 
 	//hide elements when click delete
 	$('.delete').click(function(){
-		$(this).siblings('span').slideUp();	
+		$(this).closest('.items').slideUp();	
 	});
 
 	//make list elements .items draggable
